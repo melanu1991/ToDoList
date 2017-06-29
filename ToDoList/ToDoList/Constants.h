715f73c057659b0ff6a1a009b6cbf645d67e0c82
melanu1991 <melanu284733@gmail.com>
2017-06-29 22:16:15 +0300

    Constant changed

--- ToDoList/ToDoList/Constants.h
+++ ToDoList/ToDoList/Constants.h
@@ -1,17 +1,86 @@
 #ifndef Constants_h
 #define Constants_h
 
-static NSString * const VAKTaskWasChanged = @"TaskWasChanged";
-static NSString * const VAKAddTaskTitle = @"Add task";
+static NSString * const VAKTaskWasChangedOrAddOrDelete = @"VAKTaskWasChangedOrAddOrDelete";
+
+static NSString * const VAKAddTaskTitle = @"Add Item";
 static NSString * const VAKEditTaskTitle = @"Edit task";
-static NSString * const VAKInboxCell = @"inboxCell";
+static NSString * const VAKInputNewNameGroup = @"Input new name group";
+
+
 static NSString * const VAKAddController = @"VAKAddTaskController";
-static NSString * const VAKDetailSegue = @"detailSegue";
-static NSString * const VAKKeySort = @"taskName";
-static NSString * const VAKDateTitle = @"Select date";
-static NSString * const VAKDoneTitle = @"Done";
-static NSString * const VAKDateController = @"VAKSelectDateController";
-static NSString * const VAKSaveTitle = @"Save";
-static NSString * const VAKDateFormat = @"HH:mm dd.MMMM.yyyy";
+
+static NSString * const VAKDateTitle = @"Choose date";
+
+
+static NSString * const VAKCustumCellIdentifier = @"custumCell";
+
+static NSString * const VAKTitleForHeaderCompleted = @"Completed";
+static NSString * const VAKEditButton = @"Edit";
+static NSString * const VAKDoneButton = @"Done";
+static NSString * const VAKCancelButton = @"Cancel";
+static NSString * const VAKBackButton = @"Back";
+static NSString * const VAKAddButton = @"+";
+static NSString * const VAKDelete = @"Delete";
+static NSString * const VAKOkButton = @"OK";
+static NSString * const VAKWarningDeleteMessage = @"Are you sure you want remove this item?";
+
+static NSString * const VAKDeleteTaskTitle = @"Delete task";
+static NSString * const VAKAddNewTask = @"VAKAddNewTask";
+static NSString * const VAKDeleteTask = @"VAKDeleteTask";
+static NSString * const VAKSelectedDate = @"VAKSelectedDate";
+static NSString * const VAKCurrentTask = @"VAKCurrentTask";
+static NSString * const VAKLastTaskName = @"VAKLastTaskName";
+static NSString * const VAKLastNotes = @"VAKLastNotes";
+static NSString * const VAKLastDate = @"VAKLastDate";
+static NSString * const VAKDetailTaskWasChanged = @"VAKDetailTaskWasChanged";
+static NSString * const VAKWasEditNameGroup = @"VAKWasEditNameGroup";
+static NSString * const VAKDeleteGroupTask = @"VAKDeleteGroupTasks";
+static NSString * const VAKDoneTask = @"VAKDoneTask";
+
+static NSString * const VAKAddProjectLabel = @"Add project";
+static NSString * const VAKSelectPriority = @"Select Priority";
+
+static NSString * const VAKNone = @"None";
+static NSString * const VAKLow = @"Low";
+static NSString * const VAKMedium = @"Medium";
+static NSString * const VAKHigh = @"High";
+
+static NSString * const VAKCompletedTask = @"VAKCompletedTasks";
+static NSString * const VAKNotCompletedTask = @"VAKNotCompletedTasks";
+
+static NSString * const VAKReturnKey = @"\n";
+
+static NSString * const VAKTaskNameCellIdentifier = @"VAKTaskNameCell";
+static NSString * const VAKRemindCellIdentifier = @"VAKRemindCell";
+static NSString * const VAKDateCellIdentifier = @"VAKDateCell";
+static NSString * const VAKNotesCellIdentifier = @"VAKNotesCell";
+static NSString * const VAKCustumCellNib = @"VAKCustumCell";
+static NSString * const VAKPriorityCellIdentifier = @"VAKPriorityCell";
+
+static NSString * const VAKTaskTitle = @"TASK";
+static NSString * const VAKRemindTitle = @"REMIND";
+static NSString * const VAKPriorityTitle = @"PRIORITY";
+static NSString * const VAKNotesTitle = @"NOTES";
+
+static NSString * const VAKAddProject = @"VAKAddProject";
+static NSString * const VAKNameNewProject = @"VAKNameNewProject";
+static NSString * const VAKInbox = @"Inbox";
+static NSString * const VAKToday =  @"Today";
+
+static NSString * const VAKDateFormatWithHourAndMinute = @"EEEE, dd MMMM yyyy г., H:m";
+static NSString * const VAKDateFormatWithoutHourAndMinute = @"dd.MM.YYYY";
+
+static NSString * const VAKStoriboardIdentifierTodayViewController = @"VAKStoriboardIdentifierTodayViewController";
+static NSString * const VAKTaskOfSelectedGroup = @"Tasks Of Selected Group";
+
+static NSInteger const VAKZero = 0;
+static NSInteger const VAKOne = 1;
+static NSInteger const VAKTwo = 2;
+static NSInteger const VAKThree = 3;
+static NSInteger const VAKFour = 4;
+
+static CGFloat const VAKHeightBigCell = 200.f;
+static CGFloat const VAKHeightRegularCell = 44.f;
 
 #endif
