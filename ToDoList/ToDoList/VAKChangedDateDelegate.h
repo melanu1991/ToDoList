e70a9067cd46e6f7f2ba6a4a62571325ce53947a
melanu1991 <melanu284733@gmail.com>
2017-06-30 09:36:22 +0300

    fdfsd

--- ToDoList/ToDoList/VAKChangedDateDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@protocol VAKChangedDateDelegate <NSObject>
-
-- (void)setNewDateWithDate:(NSDate *)date;
-
-@end
--- ToDoList/ToDoList/VAKSearchViewController.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface VAKSearchViewController : UIViewController
-
-@end
