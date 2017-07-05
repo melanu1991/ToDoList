c8a95da055a17fe7ab0b80e79f70d66d5a644b40
melanu1991 <melanu284733@gmail.com>
2017-07-05 13:10:43 +0300

    Создание сущности Task

---
+++ ToDoList/ToDoList/VAKTask+CoreDataClass.m
@@ -0,0 +1,15 @@
+#import "VAKTask+CoreDataClass.h"
+
+@implementation VAKTask
+
+@dynamic taskName;
+@dynamic taskId;
+@dynamic notes;
+@dynamic startedAt;
+@dynamic finishedAt;
+@dynamic priority;
+@dynamic remindMeOnADay;
+@dynamic completed;
+@dynamic currentGroup;
+
+@end
